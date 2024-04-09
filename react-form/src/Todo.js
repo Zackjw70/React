3160cb2b7051a215359c6af67714335b41159c34
Zackjw70 <zjwyeth@email.neit.edu>
2024-04-09 19:25:35 -0400

    edited assignment ... again

---
+++ react-form/src/Todo.js
@@ -0,0 +1,26 @@
+import { SignUpForm } from './form';
+import {useState} from 'react';
+
+const Todo = () => {
+    const [showModal, setShowModal] = useState(false);
+    const onDissmissModal = () => {
+        setShowModal(false)
+    }
+
+    return(
+        <>
+        <div class="card">
+        <div class="card-content">
+          <h2>Form</h2>
+          <button onClick={() => setShowModal(true)}class="btn">Start</button>
+        </div>
+      </div>
+      { showModal && <SignUpForm dismissModal={onDissmissModal}/>}
+        </>
+        
+    )
+}
+//SMall change to assignment
+//adding extra comments to the assingment
+
+export default Todo
