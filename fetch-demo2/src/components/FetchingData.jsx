a4b140d4e41d0eb94af41c06f2c755569280144c
Zackjw70 <zjwyeth@email.neit.edu>
2024-04-16 21:05:08 -0400

    InClassActivity

---
+++ fetch-demo2/src/components/FetchingData.jsx
@@ -0,0 +1,30 @@
+// Display a list of posts
+import Loading from "./Loading";
+import useFetch from "../hooks/useFetch";
+
+const FetchingData = () => {
+    const { data, error, loading } = useFetch('posts');
+  
+    if (loading) {
+      return <Loading />;
+    }
+  
+    if (error) {
+      return <p>An error occurred</p>;
+    }
+  
+    return (
+      <>
+        {data?.map((product) => {
+          return (
+            <div key={product.id}>
+              <p>{product.title}</p>
+              <p>{product.body}</p>
+            </div>
+          );
+        })}
+      </>
+    );
+  };
+  export default FetchingData;
+  
---
+++ fetch-demo2/src/components/PostingData.jsx
@@ -0,0 +1,6 @@
+// Display a list of posts
+const PostingData = () => {
+    return <div>Posting data</div>;
+  };
+  
+  export default PostingData;
